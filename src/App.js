b0a50c88cfb454890e110837ffd9e68c4813c254
JoasGomes <jojoca.vitor777@gmail.com>
2022-07-09 08:16:27 -0300

    login routes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login/*" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
---
+++ src/Components/Login/LoginCreate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginCreate = () => {
+  return <div>Login Criar</div>;
+};
+
+export default LoginCreate;
