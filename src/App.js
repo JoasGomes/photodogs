ac1255114e152dbfe8f6b03cdfa250a6198da460
JoasGomes <jojoca.vitor777@gmail.com>
2022-07-09 07:17:45 -0300

    estrutura inicial e header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
