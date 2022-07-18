243c9e47e811aa9eefaabfc4686e5ff178b0fc00
JoasGomes <jojoca.vitor777@gmail.com>
2022-07-18 12:57:41 -0300

    Perdeu Senha

--- src/api.js
+++ src/api.js
@@ -114,3 +114,29 @@ export function PHOTO_DELETE(id) {
     },
   };
 }
+
+export function PASSWORD_LOST(body) {
+  return {
+    url: API_URL + '/api/password/lost',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function PASSWORD_RESET(body) {
+  return {
+    url: API_URL + '/api/password/reset',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
