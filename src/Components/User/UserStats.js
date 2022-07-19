e57da74511371f7063f9b44506fc2fc0c5b1d18b
JoasGomes <jojoca.vitor777@gmail.com>
2022-07-19 13:18:31 -0300

    Biblioteca Externa

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { GET_STATS } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 import Head from '../Helper/Head';
+import Loading from '../Helper/Loading';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      UserStats
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
--- src/api.js
+++ src/api.js
@@ -140,3 +140,15 @@ export function PASSWORD_RESET(body) {
     },
   };
 }
+
+export function GET_STATS() {
+  return {
+    url: API_URL + '/api/stats',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    },
+  };
+}
